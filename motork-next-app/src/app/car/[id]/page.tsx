3b13ccadee09f5f9847e90ed57467be9f540e2a1
huimin chen <huimin.chen@studenti.unipd.it>
2023-09-21 14:02:28 +0200

    added more componnet to deal with the wrong id route in detailed page for cars

--- motork-next-app/src/app/car/[id]/page.tsx
+++ motork-next-app/src/app/car/[id]/page.tsx
@@ -1,22 +1,54 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import data from "../../data/data.json";
 import CarDetailCard from "../../components/Card/CarDetailCard";
+import CircularIndeterminate from "@/app/components/Components/Loading";
+import ErrorModal from "@/app/components/Components/InfoModal";
 
 export default function CarDetail({ params }: { params: { id: string } }) {
-  const selectedCar = data.find((car) => car.id === parseInt(params.id)) || {
-    id: 0,
-    make: "",
-    model: "",
-    price: 0,
-    description: "",
-    features: "",
-    mileage: 0,
-  };
+  const [selectedCar, setSelectedCar] = useState(
+    null || {
+      id: 0,
+      make: "",
+      model: "",
+      price: 0,
+      description: "",
+      features: "",
+      mileage: 0,
+    }
+  );
+  const [loading, setLoading] = useState(true);
+  const [failure, setFailure] = useState(false);
 
+  useEffect(() => {
+    // fetch data Asynchronously
+    const fetchData = async () => {
+      try {
+        const car = data.find((car) => car.id === parseInt(params.id));
+        if (car) {
+          setSelectedCar(car);
+        } else {
+          setFailure(true);
+        }
+        setLoading(false);
+      } catch (error) {
+        // catch error
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [params.id]);
   return (
-    <div className="pt-20 px-5 min-h-[calc(100vh-12rem)]">
-      {Object.keys(selectedCar).length > 0 ? (
+    <div className="pt-20 grid px-5 min-h-[calc(100vh-12rem)]">
+      {loading ? (
+        //if not loading, show loading icon
+        <div className="place-self-center">
+          <CircularIndeterminate />
+        </div>
+      ) : selectedCar ? (
         <CarDetailCard
           id={selectedCar.id}
           make={selectedCar.make}
@@ -27,7 +59,13 @@ export default function CarDetail({ params }: { params: { id: string } }) {
           mileage={selectedCar.mileage}
         />
       ) : (
-        <p>Loading...</p>
+        <p>Car not found</p>
+      )}
+      {failure && (
+        <ErrorModal
+          header={"NO INFORMATION FOUND"}
+          body={"THERE  IS NO INFORMATION FOUND UNDER THIS ID"}
+        />
       )}
     </div>
   );
